726158b58588010287c9d7a037b1407827ca0730
annrinTN <annrumsey@comcast.com>
2020-01-30 19:39:59 -0600

    question one branch

---
+++ data/LBB-question-1-ar.sql
@@ -0,0 +1,23 @@
+/* Lahman-Baseball Project Ann Rumsey due 08-Feb-2019
+Question :: 1 
+What range of years does the provided database cover?
+
+    SOURCES ::
+        * ...
+
+    DIMENSIONS ::
+        * ...
+
+    FACTS ::
+        * ...
+
+    FILTERS ::
+        * ...
+
+    DESCRIPTION ::
+        ...
+
+    ANSWER ::
+        ...
+*/
+SELECT *
