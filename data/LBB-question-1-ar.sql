a8a5a7111f660a09ef8ec4274eef5a7d6e7cb41c
annrinTN <annrumsey@comcast.com>
2020-01-30 20:35:09 -0600

    commit some of quetion one's work

--- data/LBB-question-1-ar.sql
+++ data/LBB-question-1-ar.sql
@@ -1,12 +1,12 @@
 /* Lahman-Baseball Project Ann Rumsey due 08-Feb-2019
-Question :: 1 
-What range of years does the provided database cover?
-
+Question # 1 
+  **  What range of years does the provided database cover? **
+  
     SOURCES ::
-        * ...
+        * twenty-four tables have year
 
     DIMENSIONS ::
-        * ...
+        * Year is or is not consistent as a field name
 
     FACTS ::
         * ...
@@ -20,4 +20,11 @@ What range of years does the provided database cover?
     ANSWER ::
         ...
 */
-SELECT *
+-- Get INFORMATION_SCHEMA on public tables
+--SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' LIMIT 10;
+-- Get INFORMATION_SCHEMA on base table
+SELECT table_name, column_name, column_default, data_type, ordinal_position
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA='public' 
+--AND column_name LIKE LOWER(%ear)
+ORDER BY column_name DESC;
