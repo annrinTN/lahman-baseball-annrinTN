231ca48d6e3194469da57d9c689a5d45037c153a
annrinTN <annrumsey@comcast.com>
2020-02-01 13:57:49 -0600

    question-3 shell w notes first commit

---
+++ queries/LBB-question-3-ar.sql
@@ -0,0 +1,49 @@
+/*
+QUESTION #3 ::
+   ... Find all players in the database who played at Vanderbilt University. 
+   ... Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
+   ... Sort this list in descending order by the total salary earned. 
+   ... Which Vanderbilt player earned the most money in the majors?
+
+        ...
+
+    SOURCES ::
+
+        ... people, salaries, schools, collegeplaying
+
+    DIMENSIONS ::
+
+        ... playerid firstname, lastneame from people
+		... playerid, salary from salaries
+		... playerid, schoolid from collegeplaying
+		... schoolid, schoolname from schools
+
+    FACTS ::
+
+        * ...
+
+    FILTERS ::
+
+        ... Filter to Vanderbilt from schools
+		...  link to collegeplaying to get playerid who went to Vandy
+		.... link to salaries by playerid
+
+DESCRIPTION ::
+
+        ...
+
+
+
+    ANSWER ::
+
+        ...
+
+
+
+*/
+
+
+
+SELECT *
+
+FROM ...;
