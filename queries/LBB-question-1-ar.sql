3fa3437a14c2926fa8471acabcd3536d7135f366
annrinTN <annrumsey@comcast.com>
2020-02-01 10:42:13 -0600

    finished Q1

--- queries/LBB-question-1-ar.sql
+++ queries/LBB-question-1-ar.sql
@@ -1,46 +1,40 @@
 /* Lahman-Baseball Project Ann Rumsey due 08-Feb-2019
-Question # 1 
+Question :: # 1 
   **  What range of years does the provided database cover? **
   
     SOURCES ::
-        * twenty-one or twenty-four tables have year
+        * batting, fielding, pitching tables
+		...these three are the main data tables with yearid
+		...four tables do ot have have year columns
 
     DIMENSIONS ::
-        * yearid is mostly consistent as a field name; year is used in homegames and debut is used in people
+        * ...
 
     FACTS ::
-        * ...
+        * ... yearid is mostly consistent as a field name and is an integer; 
+		... field name 'year' is used in homegames and 'debut' is used in people
 
     FILTERS ::
-        * ...
+        * ... MIN (yearid) and MAX(yearid)
 
     DESCRIPTION ::
-        ...
+        ... get year columns and min / max values from batting, fielding, pitching as the main tables mentioned in the readme
+		... Validate each table has the same
+        ... Build one row for visualization
 
-    ANSWER ::
+    ANSWER :: years 1871 to 2016
         ...
 */
--- Get INFORMATION_SCHEMA on public tables
--- Source help https://stackoverflow.com/questions/175415/how-do-i-get-list-of-all-tables-in-a-database-using-tsql
---SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE' LIMIT 30;
-
--- Get INFORMATION_SCHEMA on base table with column data
-SELECT table_name, column_name, column_default, data_type, ordinal_position
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-TABLE_SCHEMA='public' 
---AND 
---column_name = yearid
---('year', 'yearid', 'debut')
---GROUP BY columns.column_name
-ORDER BY column_name DESC, table_name;
-
-/* get year columns and values
-
-*/
-WITH year AS 
+SELECT MIN(min_year), MAX (max_year)
+FROM
 (
-	SELECT table_name, column_name, data_type
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE 
-TABLE_SCHEMA='public' 
+SELECT min(yearid) AS min_year, max(yearid) AS max_year
+FROM batting 
+UNION ALL
+SELECT min(yearid) AS min_year, max(yearid) AS max_year
+FROM fielding 
+UNION ALL
+SELECT min(yearid) AS min_year, max(yearid) AS max_year
+FROM pitching)
+AS sub; 
+
