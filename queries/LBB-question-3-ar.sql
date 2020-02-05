891d74c50d6f69321fb921220f07e05c27cc4cb2
annrinTN <annrumsey@comcast.com>
2020-02-04 20:34:06 -0600

    question 3 with duplicate playerid

--- queries/LBB-question-3-ar.sql
+++ queries/LBB-question-3-ar.sql
@@ -42,8 +42,49 @@ DESCRIPTION ::
 
 */
 
+--people query without filters gives 19,112 player names
+--attempt to use CTE to join player names from people to collegeplaying on playerid 
+--then to schools on schoolid then to salaries on player id
+/*WITH s1 AS (
+	SELECT playerid AS player, salary
+FROM salaries AS s),
+
+--s2 gives 65 rows of Vandy players
+s2 AS (
+	SELECT schoolid, playerid AS player
+FROM collegeplaying AS cp
+WHERE schoolid LIKE 'vand%'
+--INNER JOIN
+SELECT DISTINCT p.playerid AS player, namefirst, namelast
+FROM people AS p 
+
+INNER JOIN schools AS s
+ON s.playerid = p.playerid
+WHERE schoolid LIKE 'vand%' )
 
 
-SELECT *
+GROUP BY player, s.salary DESC
+*/
 
-FROM ...;
+WITH s1 AS (
+SELECT DISTINCT p.playerid AS player, namefirst, namelast
+FROM people AS p ),
+--s2 gives 65 rows of Vandy players
+s2 AS (
+	SELECT schoolid, playerid AS player
+FROM collegeplaying AS cp
+WHERE schoolid LIKE 'vand%'),
+
+s3 AS (
+	SELECT playerid AS player, salary
+FROM salaries AS s)
+
+SELECT DISTINCT s1.player, s3.salary, schoolid
+FROM s2
+INNER JOIN s1
+ON s1.player = s2.player
+INNER JOIN s3
+ON s2.player = s3.player
+
+GROUP BY s1.player, s3.salary, schoolid 
+ORDER BY s3.salary DESC;
